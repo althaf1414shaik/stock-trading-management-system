a2d81b2fc26b3dc04cecd2bb34ca9e901790d797
althaf1414shaik <althaf1414shaik@gmail.com>
2026-07-09 14:49:14 +0530

    Add files via upload
    Initial commit for Stock Trading System.

---
+++ StockTradingApplication.java
@@ -0,0 +1,13 @@
+package com.StockTradingApplication.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradingApplicationTests.java {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
